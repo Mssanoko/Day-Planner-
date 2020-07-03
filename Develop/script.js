e3296a32b8de2dd88dc535b71959c369c082d5fe
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-03 18:28:12 -0400

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
+
+// Moment declaration for day and time 
+moment(Date);
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+// Calculates the start of day
+var beforeTime = moment().startOf('day').add(9, "hours");
+
 // Time blocks
 
 var time1 = beforeTime.add(0, "h");
