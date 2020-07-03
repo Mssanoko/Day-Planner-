44e441646ec5c9abcf7f6901b30bcdb263536c8d
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-03 18:50:25 -0400

    updated timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,13 @@
 
-// Moment Current time 
-var currentTime = moment();
-
 // Moment declaration for day and time 
 moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+
+// Moment Current time 
+var currentTime = moment();
+
+// Returns current time to the nearest hour - 12:30 becomes 12:00
+currentTime = currentTime.startOf("hour");
 
 // Calculates the start of day
 var beforeTime = moment().startOf('day').add(9, "hours");
@@ -35,4 +38,24 @@ $(".block4").text(time4);
 var time5 = beforeTime.add(1, "h");
 time5 = time5.format('hh:mm A');
 $(".block5").text(time5);
+
+//2pm
+var time6 = beforeTime.add(1, "h");
+time6 = time6.format('hh:mm A');
+$(".block6").text(time6);
+
+//3pm
+var time7 = beforeTime.add(1, "h");
+time7 = time7.format('hh:mm A');
+$(".block7").text(time7);
+
+//4pm
+var time8 = beforeTime.add(1, "h");
+time8 = time8.format('hh:mm A');
+$(".block8").text(time8);
+
+//5pm
+var time9 = beforeTime.add(1, "h");
+time9 = time9.format('hh:mm A');
+$(".block9").text(time9);
 
