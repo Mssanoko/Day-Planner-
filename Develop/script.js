4886dcbb8d4b2cb254c40e7a8df1573bfda5f055
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-03 19:52:50 -0400

    time block 9am

--- Develop/script.js
+++ Develop/script.js
@@ -8,3 +8,7 @@ currentTime = currentTime.startOf("hour");
 var beforeTime = moment().startOf('day').add(9, "hours");
 
 //Time Blocks
+//9am time block
+var time1 = beforeTime.add(0, "h");
+time1 = time1.format('hh:mm A');
+$(".block1").text(time1);
