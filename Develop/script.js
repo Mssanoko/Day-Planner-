347163adadbd67fc765a12e71ee0a1358797cf2c
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-04 19:56:00 -0400

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     // listen for save button clicks
     $(".saveBtn").on("click", function() {
       // get nearby values
-      var formValue = $(this).siblings(".description").val();
-      var lisTime = $(this).parent().attr("id");
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
   
       // save in localStorage
       localStorage.setItem(time, value);
@@ -43,13 +43,12 @@ $(document).ready(function() {
     $("#hour-10 .description").val(localStorage.getItem("hour-10"));
     $("#hour-11 .description").val(localStorage.getItem("hour-11"));
     $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
-    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
-    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
-    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
-    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
   
     // display current day on page
-    moment(Date);
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
   });
