0cd82c02cd7ec83f47843096ae3d2434a6882987
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-03 20:15:05 -0400

    block 11am

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,7 @@ currentTime = currentTime.startOf("hour");
 var beforeTime = moment().startOf('day').add(9, "hours");
 
 //Time Blocks
+
 //9am time block
 var time1 = beforeTime.add(0, "h");
 time1 = time1.format('hh:mm A');
@@ -16,3 +17,9 @@ $(".block1").text(time1);
 var time2 = beforeTime.add(1, "h");
 time2 = time2.format('hh:mm A');
 $(".block2").text(time2);
+//11am time block
+var time3 = beforeTime.add(1, "h");
+time3 = time3.format('hh:mm A');
+$(".block3").text(time3);
+
+
